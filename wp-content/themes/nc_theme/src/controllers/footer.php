96049f546ba84baa5c901688a1ecf8649c6cd6f3
Bruno <betcheverry@net-com.fr>
2024-01-25 11:26:14 +0100

    🚧 Back > Footer (sans la newsletter)

--- wp-content/themes/nc_theme/src/controllers/footer.php
+++ wp-content/themes/nc_theme/src/controllers/footer.php
@@ -1,5 +1,42 @@
 <?php
 
 function nc_footer() {
-    render('footer');
+    //Menu footer
+    $menu =  wp_nav_menu([
+        'theme_location' => "menu_footer",
+        'items_wrap' => '<ul class="%2$s">%3$s</ul>',
+        'depth' => 2,
+        'echo' => false,
+        'container' => '',
+    ]);
+
+    //Accès rapides
+    $acces = [];
+    $acces_rapide = get_field("acces_rapides", "options") ?? null;
+
+    if(!empty($acces_rapide)){
+        foreach ($acces_rapide as $value) {
+            $acces[$value['lien'][0]->ID] = $value['lien'][0]->post_name ?? null;
+        }
+    }
+
+    //Réseaux sociaux
+    $social = [];
+    $social_networks = get_field("reseaux_sociaux", "options") ?? null;
+
+    if(!empty($social_networks)){
+        $social["linkedin"] = $social_networks['linkedin'] ?? null;
+        $social["facebook"] = $social_networks['facebook'] ?? null;
+        $social["instagram"] = $social_networks['instagram'] ?? null;
+        $social["x"] = $social_networks['x'] ?? null;
+    }
+
+
+    render('footer', [
+        "logo" => get_field("logo_footer", "options") ?? null,
+        "social" => $social ?? null,
+        "acces_rapide" => $acces ?? null,
+        "coordonnees" => get_field("coordonnees_localisation", "options") ?? null,
+        "menu" => $menu ?? null,
+    ]);
 }
--- wp-content/themes/nc_theme/src/views/footer.php
+++ wp-content/themes/nc_theme/src/views/footer.php
@@ -1,9 +1,56 @@
 <?php
 /**
  * @see nc_footer()
+ * @var string $logo
+ * @var array $social
+ * @var array $acces_rapide
+ * @var string $coordonnees
+ * @var string $menu
+ *
  */
 ?>
 
 <footer>
+    <?php if (!empty($logo)) : ?>
+        <img src="<?= wp_get_attachment_image_url($logo['ID'], "nc_footer") ?>" alt="">
+    <?php endif; ?>
+
+    <?php if(!empty($menu)) : ?>
+        <?= $menu ?>
+    <?php endif; ?>
+
+    <?php if(!empty($coordonnees)) : ?>
+       <p><?= $coordonnees ?></p>
+    <?php endif; ?>
+
+    <div>
+        <h3>Réseaux sociaux</h3>
+
+        <?php if(!empty($social["linkedin"])) : ?>
+            <a href="<?= $social["linkedin"] ?>" target="_blank">Linkedin</a>
+        <?php endif; ?>
+
+        <?php if(!empty($social["facebook"])) : ?>
+            <a href="<?= $social["facebook"] ?>" target="_blank">Facebook</a>
+        <?php endif; ?>
+
+        <?php if(!empty($social["instagram"])) : ?>
+            <a href="<?= $social["instagram"] ?>" target="_blank">Instagram</a>
+        <?php endif; ?>
+
+        <?php if(!empty($social["x"])) : ?>
+            <a href="<?= $social["x"] ?>" target="_blank">X</a>
+        <?php endif; ?>
+
+        <hr>
+    </div>
+
+    <?php if(!empty($acces_rapide)) : ?>
+        <?php foreach ($acces_rapide as $id => $value) : ?>
+            <div>
+                <a href="<?= get_permalink($id) ?? null ?>"><?= ucfirst($value) ?? null ?></a>
+            </div>
+        <?php endforeach; ?>
+    <?php endif; ?>
 
 </footer>
