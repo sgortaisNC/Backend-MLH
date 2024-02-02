42fb8dc5e0419a7ad43554fabe7a0d1239f09ca2
Bruno <betcheverry@net-com.fr>
2024-02-02 11:51:37 +0100

    🚧 Back > Homepage : test api

---
+++ wp-content/plugins/nc-api/nc_api.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * NC Api
+ *
+ * Plugin Name: NC Api
+ * Plugin URI:  https://www.net-com.fr/
+ * Description: Création de l'api du projet
+ * Version:     0.1
+ * Author:      Bruno Etcheverry [Net.Com]
+ */
+
+
+
+add_action( 'rest_api_init', 'hello_world' );
+
+function hello_world()
+{
+    register_rest_route( 'montlucon/v1', '/hello', array(
+        'methods' => 'GET',
+        'callback' => 'nc_hello_world',
+    ) );
+}
+
+function nc_hello_world()
+{
+    return 'Hello World';
+}
