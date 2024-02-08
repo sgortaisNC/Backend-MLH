75fd2570c651410ffa5531e1f51e18086b3843c8
Bruno <betcheverry@net-com.fr>
2024-02-08 11:49:48 +0100

    🚧 Back > Class Page modification de l'envoi des forms

--- wp-content/plugins/nc-api/classes/Page.php
+++ wp-content/plugins/nc-api/classes/Page.php
@@ -19,7 +19,7 @@ class Page
                 'titre' => get_the_title($id),
                 'image' => (has_post_thumbnail() ? get_the_post_thumbnail_url($id, 'nc_page_single') :
                     wp_get_attachment_image_src(IMAGE_DEFAUT, 'nc_page_single')[0]),
-                'contenu' => get_the_content(null, false, $id) ?? null,
+                'contenu' => apply_shortcodes( get_the_content(null, false, $id)) ?? null,
                 'chapo' => has_excerpt($id) ? get_the_excerpt($id) : null,
                 'lien' => get_permalink($id),
             ];
