bdbae819b829de3841c5a29dc69fc07cce7ed6d8
Sébastien <sgortais@net-com.fr>
2024-02-08 14:13:24 +0100

    :sparkles: Test submission mail

--- wp-content/plugins/nc-api/classes/Formulaire.php
+++ wp-content/plugins/nc-api/classes/Formulaire.php
@@ -25,9 +25,9 @@ class Formulaire
     {
         $form_id = $_GET['id'] ?? null;
 //        $submission_data = Forminator_API::get_form( $form_id );
-        $submission_data = do_shortcode( '[forminator_form id="'.$form_id.'"]');
+        $submission_data = do_shortcode('[forminator_form id="' . $form_id . '"]');
 
-        return rest_ensure_response( $submission_data);
+        return rest_ensure_response($submission_data);
     }
 
     static function submit_form()
@@ -43,6 +43,34 @@ class Formulaire
             ];
         }
 
-        return Forminator_API::add_form_entry($id, $data);
+
+        $entry_id = Forminator_API::add_form_entry($id, $data);
+
+        if (is_numeric($entry_id)) {
+            $forminator_mail_sender = new Forminator_CForm_Front_Mail();
+            $entry = new Forminator_Form_Entry_Model($entry_id);
+            if (empty($entry->form_id) || !empty($entry->draft_id)) {
+                wp_send_json_error(esc_html__('Entry ID was not found.', 'forminator'));
+            }
+            $module_id = $entry->form_id;
+
+            Forminator_Front_Action::$module_id = $module_id;
+            Forminator_Front_Action::$module_object = Forminator_Base_Form_Model::get_model($module_id);
+            // Emulate Forminator_Front_Action::$prepared_data.
+            Forminator_Front_Action::$prepared_data = recreate_prepared_data(Forminator_Front_Action::$module_object, $entry);
+            // Emulate Forminator_Front_Action::$hidden_fields.
+            if (!Forminator_Front_Action::$module_object) {
+                wp_send_json_error(esc_html__('Error: Module object is corrupted!', 'forminator'));
+            }
+            Forminator_Front_Action::$module_settings = method_exists(Forminator_Front_Action::$module_object, 'get_form_settings')
+                ? Forminator_Front_Action::$module_object->get_form_settings() : Forminator_Front_Action::$module_object->settings;
+            Forminator_CForm_Front_Action::check_fields_visibility();
+
+            $module_object = Forminator_Base_Form_Model::get_model($module_id);
+            $forminator_mail_sender->process_mail($module_object, $entry);
+            return $entry_id;
+        } else {
+            return false;
+        }
     }
 }
