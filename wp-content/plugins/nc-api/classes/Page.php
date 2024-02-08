48ddc35833c3150598c75014f06ec87d420e65ee
Sébastien <sgortais@net-com.fr>
2024-02-08 13:17:28 +0100

    :sparkles: Route page single avec Formulaire

--- wp-content/plugins/nc-api/classes/Page.php
+++ wp-content/plugins/nc-api/classes/Page.php
@@ -5,23 +5,36 @@
  */
 class Page
 {
-    static function getOneBySlug($request) : array
+    static function getOneBySlug($request): array
     {
-        
+
         $slug = $request['slug'];
 
-        $pageBySlug = get_page_by_path( $slug, OBJECT, 'page' );
+        $pageBySlug = get_page_by_path($slug, OBJECT, 'page');
         $page = [];
-        if ( $pageBySlug ) {
+        if ($pageBySlug) {
+
+            $shortcode_id = null;
+            $pattern = '/\[forminator_form id="(\d+)"\]/';
+            $text = $pageBySlug->post_content;
+            if (preg_match($pattern, $text, $matches)) {
+                // $matches[0] contains the entire shortcode, $matches[1] contains the ID
+                $shortcode_id = $matches[1];
+
+                // Remove the shortcode from the text
+                $text = preg_replace($pattern, '', $text);
+            }
+
             $id = $pageBySlug->ID;
             $page[] = [
                 'id' => $id,
                 'titre' => get_the_title($id),
                 'image' => (has_post_thumbnail() ? get_the_post_thumbnail_url($id, 'nc_page_single') :
                     wp_get_attachment_image_src(IMAGE_DEFAUT, 'nc_page_single')[0]),
-                'contenu' => apply_shortcodes( get_the_content(null, false, $id)) ?? null,
+                'contenu' => $text ?? null,
                 'chapo' => has_excerpt($id) ? get_the_excerpt($id) : null,
                 'lien' => get_permalink($id),
+                'formulaire' => $shortcode_id ? Forminator_API::get_form_wrappers($shortcode_id) : null,
             ];
         }
 
